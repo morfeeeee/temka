ce7c92e84ce3372226e35be0a60b5eb262eceddd
serega <seregaprogrammist@yandex.ru>
2024-06-24 21:44:00 +0300

    Кнопка WebSolutions теперь кликабельная. Добавил страницу контакты и отзывы

--- src/main/kotlin/zela/cobble/web/handlers/HomeHandler.kt
+++ src/main/kotlin/zela/cobble/web/handlers/HomeHandler.kt
@@ -7,6 +7,7 @@ import zela.cobble.web.models.HomePageVM
 class HomeHandler(val renderer: ContextAwareViewRender): HttpHandler {
     override fun invoke(request: Request): Response {
         val viewModel = HomePageVM()
+        val a = 0
         return Response(Status.OK).with(renderer(request) of viewModel)
 
 
